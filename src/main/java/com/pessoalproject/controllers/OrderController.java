56276f2bb72bdf2f725de17a08cb8ce74e129fbd
brunogabriel99 <brunogabriel92.bg@gmail.com>
2021-11-24 19:05:22 -0300

    ISO 8601 and JsonIgnore

--- src/main/java/com/pessoalproject/controllers/OrderController.java
+++ src/main/java/com/pessoalproject/controllers/OrderController.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pessoalproject.entities.User;
-import com.pessoalproject.services.UserService;
+import com.pessoalproject.entities.Order;
+import com.pessoalproject.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserController {
+@RequestMapping(value = "/orders")
+public class OrderController {
 	
 	@Autowired
-	private UserService service;
+	private OrderService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = service.findAll();
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping("{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User u = service.findById(id);
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order u = service.findById(id);
 		return ResponseEntity.ok().body(u);
 	}
 }

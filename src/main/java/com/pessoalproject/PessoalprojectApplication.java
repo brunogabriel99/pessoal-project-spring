66052f94be03dedbd72679a6ded620b9306a1bb8
brunogabriel99 <brunogabriel92.bg@gmail.com>
2021-11-24 14:27:09 -0300

    Project created

---
+++ src/main/java/com/pessoalproject/PessoalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pessoalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pessoalproject/PessoalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pessoalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f9cd021808244dd3ce2fb44008dafb92cd652a4d
brunogabriel99 <brunogabriel92.bg@gmail.com>
2021-11-24 14:47:48 -0300

    User entity and controller

---
+++ src/main/java/com/pessoalproject/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.pessoalproject.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pessoalproject.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "Maria@gmail.com", "999999", "12345"); 
+		return ResponseEntity.ok().body(u);
+	}
+
+}

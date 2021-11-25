5a65d4459eb74526153feb68ca28bc77239227ec
brunogabriel99 <brunogabriel92.bg@gmail.com>
2021-11-25 14:34:52 -0300

    Payment, one-to-one association

--- src/main/java/com/pessoalproject/config/TestConfig.java
+++ src/main/java/com/pessoalproject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pessoalproject.entities.Category;
 import com.pessoalproject.entities.Order;
 import com.pessoalproject.entities.OrderItem;
+import com.pessoalproject.entities.Payment;
 import com.pessoalproject.entities.Product;
 import com.pessoalproject.entities.User;
 import com.pessoalproject.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
